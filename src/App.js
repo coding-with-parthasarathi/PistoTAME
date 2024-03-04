5ec683f825d7cc7ca4e6cd2d2883a0c683baabe2
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-03-05 00:32:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 //import "./App.css";
 import Navbar from "./components/Navbar";
 import {
-	BrowserRouter as Router,
+	HashRouter as Router,
 	Routes,
 	Route,
 } from "react-router-dom";
